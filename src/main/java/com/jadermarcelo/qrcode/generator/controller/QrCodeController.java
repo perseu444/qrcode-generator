85263c3a795495dea4352ab0b20d1c7477a4d406
Jader Marcelo <jadermarcelo95@gmail.com>
2025-05-04 21:53:45 -0300

    first controller

---
+++ src/main/java/com/jadermarcelo/qrcode/generator/controller/QrCodeController.java
@@ -0,0 +1,19 @@
+package com.jadermarcelo.qrcode.generator.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/qrcode")
+public class QrCodeController {
+
+    @PostMapping
+    public ResponseEntity<> generate(@RequestBody){
+
+
+
+    }
+}
