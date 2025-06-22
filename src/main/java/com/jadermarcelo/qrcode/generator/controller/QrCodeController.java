7bebcc03afd34073134187d624690bae41b12ecc
Jader Marcelo <jadermarcelo95@gmail.com>
2025-06-21 21:42:33 -0300

    Dockerfile + AWS config

--- src/main/java/com/jadermarcelo/qrcode/generator/controller/QrCodeController.java
+++ src/main/java/com/jadermarcelo/qrcode/generator/controller/QrCodeController.java
@@ -2,6 +2,7 @@ package com.jadermarcelo.qrcode.generator.controller;
 
 import com.jadermarcelo.qrcode.generator.dto.QrCodeGenerateResponse;
 import com.jadermarcelo.qrcode.generator.dto.QrCodeGeneratorRequest;
+import com.jadermarcelo.qrcode.generator.service.QrCodeGeneratorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +13,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/qrcode")
 public class QrCodeController {
 
+    private final QrCodeGeneratorService qrCodeGeneratorService;
+
+    public QrCodeController(QrCodeGeneratorService qrCodeService) {
+        this.qrCodeGeneratorService = qrCodeService;
+    }
+
+
     @PostMapping
     public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGeneratorRequest request){
-        return null;
+
+
+        try {
+
+            QrCodeGenerateResponse response = this.qrCodeGeneratorService.generateAndUploadQrCode(request.text());
+            return ResponseEntity.ok(response);
+
+
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+
+        }
+
+
+
 
 
 
